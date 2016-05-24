2345731d97326554dafe86a0d06285573c8b6391
k.kaszubski <kamcio181@o2.pl>
2016-05-24 16:50:08 +0200

    ini

--- app/src/main/java/com/example/kamil/connectioncounteraudio66bts/ConnectionReceiver.java
+++ app/src/main/java/com/example/kamil/connectioncounteraudio66bts/ConnectionReceiver.java
@@ -16,9 +16,8 @@ public class ConnectionReceiver extends BroadcastReceiver {
     private static final String TAG = "ConnectionReceiver";
     private static final String PREFS_NAME = "PrefsName";
     private static final String TIME_KEY = "TimeKey";
-    private long startMillis;
-    private boolean stop = false;
-    private android.os.Handler handler = new android.os.Handler();
+    private static Runnable runnable;
+    private static final android.os.Handler handler = new android.os.Handler();
     public ConnectionReceiver() {
     }
 
@@ -28,78 +27,35 @@ public class ConnectionReceiver extends BroadcastReceiver {
         if(intent != null && intent.getAction() !=null){
             String action = intent.getAction();
             switch (action){
-                case BluetoothHeadset.ACTION_AUDIO_STATE_CHANGED:
-                    Log.e(TAG, "audio state changed");
-                    if(!((BluetoothDevice)(intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE))).getAddress().equals("00:1A:7D:E0:35:5F"))
-                        break;
-                    Log.e(TAG, "66 Audio BTS");
-                    int currentState = intent.getIntExtra(BluetoothHeadset.EXTRA_STATE, 0);
-                    int previousState = intent.getIntExtra(BluetoothHeadset.EXTRA_PREVIOUS_STATE, 0);
-                    if(currentState != previousState) {
-                        Log.e(TAG, "state changed");
-                        if (currentState == BluetoothHeadset.STATE_AUDIO_CONNECTED) {
-                            Log.e(TAG, "connected");
-                            stop = false;
-                            Calendar calendar = Calendar.getInstance();
-                            startMillis = calendar.getTimeInMillis();
-                            Runnable runnable = new Runnable() {
-                                @Override
-                                public void run() {
-                                    Log.e(TAG, "saveTime");
-                                    if(!stop){
-                                        Calendar calendar2 = Calendar.getInstance();
-                                        SharedPreferences preferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-                                        preferences.edit().putLong(TIME_KEY, preferences.getLong(TIME_KEY, 0) + startMillis - calendar2.getTimeInMillis()).apply();
-                                        handler.postDelayed(this, 1000);
-                                    } else
-                                        stop = false;
-                                }
-                            };
-                            handler.postDelayed(runnable,1000);
-                        } else if (currentState == BluetoothHeadset.STATE_AUDIO_DISCONNECTED) {
-                            Log.e(TAG, "disconnected");
-                            stop = true;
-                            Calendar calendar2 = Calendar.getInstance();
-                            SharedPreferences preferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-                            preferences.edit().putLong(TIME_KEY, preferences.getLong(TIME_KEY, 0) + startMillis - calendar2.getTimeInMillis()).apply();
-                        }
-                    }
-                    break;
-                case BluetoothHeadset.ACTION_CONNECTION_STATE_CHANGED:
                 case BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED:
                     Log.e(TAG, "Adapter connection state changed");
                     if(!((BluetoothDevice)(intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE))).getAddress().equals("00:1A:7D:E0:35:5F"))
                         break;
                     Log.e(TAG, "66 Audio BTS");
-                    int currentState2 = intent.getIntExtra(BluetoothHeadset.EXTRA_STATE, 0);
-                    int previousState2 = intent.getIntExtra(BluetoothHeadset.EXTRA_PREVIOUS_STATE, 0);
+                    int currentState2 = intent.getIntExtra(BluetoothAdapter.EXTRA_CONNECTION_STATE, 0);
+                    int previousState2 = intent.getIntExtra(BluetoothAdapter.EXTRA_PREVIOUS_CONNECTION_STATE, 0);
                     if(currentState2 != previousState2) {
                         Log.e(TAG, "state changed");
-                        if (currentState2 == BluetoothHeadset.STATE_AUDIO_CONNECTED) {
+                        if (currentState2 == BluetoothAdapter.STATE_CONNECTED) {
                             Log.e(TAG, "connected");
-                            stop = false;
-                            Calendar calendar = Calendar.getInstance();
-                            startMillis = calendar.getTimeInMillis();
-                            Runnable runnable = new Runnable() {
+                            runnable = new Runnable() {
                                 @Override
                                 public void run() {
-                                    Log.e(TAG, "saveTime");
-                                    if(!stop){
-                                        Calendar calendar2 = Calendar.getInstance();
-                                        SharedPreferences preferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-                                        preferences.edit().putLong(TIME_KEY, preferences.getLong(TIME_KEY, 0) + startMillis - calendar2.getTimeInMillis()).apply();
-                                        handler.postDelayed(this, 1000);
-                                    } else
-                                        stop = false;
+                                    Log.e(TAG, "saveTime ");
+
+                                    SharedPreferences preferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+                                    long time = preferences.getLong(TIME_KEY, 0) + 1;
+                                    preferences.edit().putLong(TIME_KEY, time).apply();
+                                    if (MainActivity.context != null)
+                                        MainActivity.context.postResult(time);
+                                    handler.postDelayed(this, 1000);
+
                                 }
                             };
-                            handler.postDelayed(runnable,1000);
-                        } else if (currentState2 == BluetoothHeadset.STATE_AUDIO_DISCONNECTED) {
+                            handler.post(runnable);
+                        } else if (currentState2 == BluetoothAdapter.STATE_DISCONNECTED) {
                             Log.e(TAG, "disconnected");
-                            stop = true;
-                            Calendar calendar2 = Calendar.getInstance();
-                            SharedPreferences preferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-                            preferences.edit().putLong(TIME_KEY, preferences.getLong(TIME_KEY, 0) + startMillis - calendar2.getTimeInMillis()).apply();
+                            handler.removeCallbacks(runnable);
                         }
                     }
                     break;
--- app/src/main/java/com/example/kamil/connectioncounteraudio66bts/MainActivity.java
+++ app/src/main/java/com/example/kamil/connectioncounteraudio66bts/MainActivity.java
@@ -1,33 +1,28 @@
 package com.example.kamil.connectioncounteraudio66bts;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    public static MainActivity context = null;
     private static final String PREFS_NAME = "PrefsName";
     private static final String TIME_KEY = "TimeKey";
-    private Button button, button2;
+    private Button button;
     private TextView textView;
-    private SharedPreferences preferences;
+    private SharedPreferences preferences; //TODO set current value, check if music is playing, change interval time and save time on disconnect
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.textView);
         button = (Button) findViewById(R.id.button);
-        button2 = (Button) findViewById(R.id.button2);
         button.setOnClickListener(this);
-        button2.setOnClickListener(this);
 
         preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
     }
@@ -35,13 +30,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onStart() {
         super.onStart();
-//        Calendar calendar = Calendar.getInstance();
-//        calendar.setTimeInMillis();
-        long time = preferences.getLong(TIME_KEY, 0);
-        int hours = (int)time/3600000;
-        int minutes = (int) (time/600000-hours*60);
-        textView.setText(hours + " : " + minutes);
+        postResult(preferences.getLong(TIME_KEY, 0));
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        context = null;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        context = this;
+    }
+
+    public void postResult(long totalSeconds){
+        long hours = TimeUnit.SECONDS.toHours(totalSeconds);
+        long minutes = TimeUnit.SECONDS.toMinutes(totalSeconds) - hours*60;
+        long seconds = TimeUnit.SECONDS.toSeconds(totalSeconds) - minutes*60;
+        textView.setText(String.format("%d:%d:%d", hours, minutes, seconds));
     }
 
     @Override
@@ -49,14 +57,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()){
             case R.id.button:
                 preferences.edit().remove(TIME_KEY).apply();
-                break;
-            case R.id.button2:
-                BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
-                ArrayList<BluetoothDevice> list = new ArrayList<>();
-                list.addAll(adapter.getBondedDevices());
-                for(BluetoothDevice b : list){
-                    Log.e("Bond", b.getAddress() + " name " + b.getName());
-                }
+                postResult(0);
                 break;
         }
     }
